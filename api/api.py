a41e0b3b2348cde16e9ebad21cc4a3e231317970
snehamis <snehamis@cisco.com>
2020-01-13 15:29:05 +0530

    Add support to cookie and test nested

--- api/api.py
+++ api/api.py
@@ -3,9 +3,23 @@ import flask
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
+@app.route('/grafana', methods=['GET'])
+def home_grafana():
+    resp = flask.make_response()
+    template = "<h1>Distant Reading Archive</h1><p>This is a check for nested cookie</p>"
+    resp = flask.make_response(template)
+    return resp
+
+#@app.route('/get-cookie/')
+#def get_cookie():
+#    username = request.cookies.get('name')
 
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+    template = "<h1>Distant Reading Archive</h1><a href=http://127.0.0.1:5000/grafana>Visit grafana</a><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+    resp = flask.make_response(template)
+    resp.set_cookie("root", "root-cookie", path="/")
+    resp.set_cookie("root-grafana", "root-grafana-cookie", path="/grafana")
+    return resp
 
 app.run()
